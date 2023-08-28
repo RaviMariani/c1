556b9f03d3e88204e8d7e835bced0fe41a2b59cc
RaviMariani <143418014+RaviMariani@users.noreply.github.com>
2023-08-28 11:07:27 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+#Faça um programa que receba dois numeros e mostre qual deles ´ e o maior
+n1 = int(input("Digite a primeira nota: "))
+n2 = int(input("Digite a segunda nota: "))
+n3 = int(input("Digite a terceira nota: "))
+n4 = int(input("Digite a quarta nota: "))
+m = (n1 + n2 + n3 + n4 ) / 4
+if m > 7 print("você foi aprovado!")
+else print("Você foi reprovado")
+           
